c30539343aaf7ebf6d69c76d21d277f0c47ad08a
Damien Nivault <damien.nivault@supinternet.fr>
2018-01-07 13:00:08 +0100

    add grade for good student with modal, close WIP

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,25 +1,29 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal')
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementsByClassName("myBtn")
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
+var span = document.getElementsByClassName("close")[0]
+var contentName = document.getElementsByClassName('name')
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+var close
+for (var i = 0; i < btn.length; i++) {
+    var anchor = btn[i]
+    anchor.onclick = function () {
+        this.nextElementSibling.style.display = "block"
+        var content = this.previousElementSibling.previousElementSibling.innerHTML
+        contentName.innerHTML = content
 
+    }
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = "none"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+        modal.style.display = "none"
 }
